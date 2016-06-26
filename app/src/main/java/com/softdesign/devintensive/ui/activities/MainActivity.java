4e9d21a5d2a1c73770184db31f3f16ac327b0170
plmax <pletnikov.m@yandex.ru>
2016-06-27 01:50:00 +0300

    Добавлен класс BaseActivity, настроены методы showProgress, hideProgress, showError, showToast. Протестирована работы методов showProgress и hideProgress.

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,56 +1,89 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
 
-    private static final String TAG= ConstantManager.TAG_PREFIX + "Main Activity";
+    private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+
+    private ImageView mCallImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG,"onCreate");
+        Log.d(TAG, "onCreate");
+
+        mCallImg = (ImageView) findViewById(R.id.call_img);
+        mCallImg.setOnClickListener(this);
+
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG,"onStart");
+        Log.d(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG,"onResume");
+        Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG,"onPause");
+        Log.d(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG,"onStop");
+        Log.d(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"onDestroy");
+        Log.d(TAG, "onDestroy");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG,"onRestart");
+        Log.d(TAG, "onRestart");
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.call_img:
+                    /*showProgress();
+                    runWithDelay();*/
+                break;
+        }
+    }
+
+    /*private void runWithDelay() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Выплнить с задержкой
+
+                hideProgress();
+            }
+        }, 6000);
+    }*/
+
 }
